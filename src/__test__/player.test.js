fa0cf0a36425d9f67c1eb2d4c9a569720957a4b4
AJ Lobb <ajlobb@live.com>
2022-03-02 14:21:19 +0000

    Knocked 2 pins passes

--- src/__test__/player.test.js
+++ src/__test__/player.test.js
@@ -5,6 +5,20 @@ const {
 describe('Get player points', () => {
     it('Given a player has rolled no balls their score should be 0', () => {
         const player = new BowlingPlayer();
-        expect(player.getPlayerPoints()).toEqual(0)
+        expect(player.getPlayerPins()).toEqual(0)
+    });
+});
+
+describe('Player Scored 1', () => {
+    it('Given a player has rolled a ball and knocked 1 pin they get 1 point', () => {
+        const player = new BowlingPlayer();
+        expect(player.playerKnocked1Pin()).toEqual(1)
+    });
+});
+
+describe('Player Scored 2', () => {
+    it('Given a player has rolled a ball and knocked 1 pin they get 1 point', () => {
+        const player = new BowlingPlayer();
+        expect(player.playerKnocked2Pins()).toEqual(2)
     });
 });
--- src/player.js
+++ src/player.js
@@ -1,10 +1,20 @@
+let totalPins = 0
 class BowlingPlayer {
-    points = 0;
-    getPlayerPoints = () => {
-        return this.points
+    pins = 0;
+    getPlayerPins = () => {
+        return this.pins
     }
+    playerKnocked1Pin = () => {
+        return this.pins + 1
+    }
+    playerKnocked2Pins = () => {
+        return this.pins + 2
+    }
+    
 }
 
 module.exports = {
     BowlingPlayer
 }
+
+
